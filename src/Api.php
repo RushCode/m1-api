c4c942a06ad301d8d93eac523f48416d7c95f481
Leo Cata <1302055+RushCode@users.noreply.github.com>
2017-10-17 20:01:02 +0300

    Release quick send methods.

--- src/Api.php
+++ src/Api.php
@@ -7,6 +7,10 @@ use leocata\M1\Abstracts\RequestMethods;
 use leocata\M1\Exceptions\MethodNotFound;
 use leocata\M1\Exceptions\MissingMandatoryField;
 use leocata\M1\InternalFunctionality\DummyLogger;
+use leocata\M1\Methods\Request\ContactAccept;
+use leocata\M1\Methods\Request\MessageDelivered;
+use leocata\M1\Methods\Request\MessageTyped;
+use leocata\M1\Methods\Request\SendMessage;
 use Psr\Log\LoggerInterface;
 
 /**
@@ -14,8 +18,18 @@ use Psr\Log\LoggerInterface;
  */
 class Api
 {
+    /**
+     * @var DummyLogger|LoggerInterface
+     */
     protected $logger;
+    /**
+     * @var
+     */
     private $client;
+    /**
+     * @var
+     */
+    private $auth;
 
     /**
      * @param LoggerInterface $logger
@@ -62,18 +76,107 @@ class Api
      * Performs the request to the Api servers.
      *
      * @param RequestMethods $method
-     * @param Authorization  $auth
      *
      * @return RequestMethods
      */
-    public function sendRequest(RequestMethods $method, Authorization $auth)
+    public function sendRequest(RequestMethods $method)
     {
-        $this->client = new HttpClient($auth);
+        $this->client = new HttpClient($this->auth);
         $response = $this->client->getResponseContent($method->getRequestString());
         if (!empty($response)) {
             $method->setResult($response);
         }
 
         return $method;
+    }
+
+    /**
+     * Quick send new message.
+     *
+     * @param $destination
+     * @param $code
+     * @param $type
+     * @param $content
+     *
+     * @return RequestMethods
+     */
+    public function sendMessage($destination, $code, $type, $content)
+    {
+        $message = new SendMessage();
+        $message->dest = $destination;
+        $message->code = $code;
+        $message->code = $type;
+        $message->code = $content;
+
+        return $this->sendRequest($message);
+    }
+
+    /**
+     * Quick send message delivery status.
+     *
+     * @param $sessionId
+     * @param $messageId
+     *
+     * @return RequestMethods
+     */
+    public function messageDelivered($sessionId, $messageId)
+    {
+        $messDelivered = new MessageDelivered();
+        $messDelivered->sessionid = $sessionId;
+        $messDelivered->messageid = $messageId;
+
+        return $this->sendRequest($messDelivered);
+    }
+
+    /**
+     * Send message type indicator
+     *
+     * @param $sessionId
+     * @param $userId
+     *
+     * @return RequestMethods
+     */
+    public function messageTyped($sessionId, $userId)
+    {
+        $messageTyped = new MessageTyped();
+        $messageTyped->sessionid = $sessionId;
+        $messageTyped->userid = $userId;
+
+        return $this->sendRequest($messageTyped);
+    }
+
+    /**
+     * Send contact Accept.
+     *
+     * @param $userId
+     * @param $message
+     * @param $state
+     *
+     * @return RequestMethods
+     */
+    public function contactAccept($userId, $message, $state)
+    {
+        $contactAccept = new ContactAccept();
+        $contactAccept->userid = $userId;
+        $contactAccept->message = $message;
+        $contactAccept->state = $state;
+
+        return $this->sendRequest($contactAccept);
+    }
+
+    /**
+     * @return Authorization
+     */
+    public function getAuth() : Authorization
+    {
+        return $this->auth;
+    }
+
+    /**
+     * @param mixed Authorization
+     */
+    public function setAuth(Authorization $auth)
+    {
+        $this->auth = $auth;
     }
 }
